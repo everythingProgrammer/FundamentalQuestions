a408d2fc8a645167bc7b94577d2759c0109449d2
Abhinav Rana <ranaabhinav50@gmail.com>
2020-12-19 12:37:25 +0530

    Graph is a tree

--- trees/Tree.java
+++ trees/Tree.java
@@ -710,7 +710,17 @@ public class Tree {
 		 
 		 
 	  }
+
 	  
+	  /*Check if given graph is tree or not 
+	   * 
+	   * A given undirected graph is a tree if - 
+	   * 	
+	   * 	*a there are no cycles in graph 
+	   * 	*b the graph is connected*/
+	  /*Link*/
+	  /*   https://www.geeksforgeeks.org/check-given-graph-tree/#:~:text=Since%20the%20graph%20is%20undirected,graph%20is%20connected%2C%20otherwise%20not.
+	  */
 	   
 }
 	
